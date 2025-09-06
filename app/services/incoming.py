8cce93fef4efeae13e2ec073b1f9f17e53370476
Pawtang <ben.dibuz@gmail.com>
2025-09-06 14:11:38 -0500

    remove env lol

--- app/services/incoming.py
+++ app/services/incoming.py
@@ -4,9 +4,15 @@ from firebase_admin import firestore
 from twilio.twiml.messaging_response import MessagingResponse
 db = firestore.client()
 
+def parse_response(response: UserMessage):
+        goal_list = response.split(";")
+
 def save_user_response(reponse: UserMessage):
     db.collection("user_responses").add(asdict(reponse))
 
 def get_user_goals(user: User):
     db.collection("goals")
     
+def save_user_goals(response: UserMessage):
+    parsed_res = parse_response(response)
+    db.collection("goals").add(asdict(parsed_res))
