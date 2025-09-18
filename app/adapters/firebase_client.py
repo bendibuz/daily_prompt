f036aa12401791504455cc806c18d21bdf25069a
Ben Dibuz <ben.dibuz@gmail.com>
2025-09-17 20:20:02 -0500

    credentials load

--- app/adapters/firebase_client.py
+++ app/adapters/firebase_client.py
@@ -2,9 +2,10 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 from app.config import settings
+import json
 
 def init_firebase():
-    cred = credentials.Certificate(settings.GOOGLE_APPLICATION_CREDENTIALS)
+    cred = credentials.Certificate(json.loads(os.environ["TEST_VAR"]))
     app = firebase_admin.initialize_app(cred)
     return app
 
